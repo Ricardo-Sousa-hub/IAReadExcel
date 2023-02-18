e3c1e027801502889c8c9a0dee77f7a5d84bb28e
Ricardo <satao.net@hotmail.com>
2023-02-18 17:21:12 +0000

    Final Beta

---
+++ Utils.py
@@ -0,0 +1,218 @@
+import os
+from collections import deque
+from datetime import datetime
+import glob
+import pandas as pd
+import openpyxl
+
+
+def reverseText(s):
+    # base case
+    if not s:
+        return s
+
+    # `s[low…high]` forms a word
+    low = high = 0
+
+    # create an empty stack
+    stack = deque()
+
+    # scan the text
+    for i, c in enumerate(s):
+        # if space is found, we found a word
+        if c == ' ':
+            # push each word into the stack
+            stack.append(s[low:high + 1])
+
+            # reset `low` and `high` for the next word
+            low = high = i + 1
+        else:
+            high = i
+
+    # push the last word into the stack
+    stack.append(s[low:])
+
+    # construct the string by following the LIFO order
+    sb = ""
+    while stack:
+        sb += stack.pop() + ' '
+
+    return sb[:-1]  # remove last space
+
+
+def GetValidDatas(sleepScore):
+    lista = []
+    for item in sleepScore:
+        itemT = item['timestamp'].split('T')
+        itemT[0] = itemT[0].replace('-', ' ')
+        itemT[0] = reverseText(itemT[0])
+        itemT[0] = itemT[0].replace(' ', '-')
+        lista.append(itemT[0])
+
+    lista.reverse()
+
+    return lista
+
+
+def EscreverCabecalho(worksheet):
+    worksheet.write(0, 0, 'Dia_Sessao')
+    worksheet.write(0, 1, 'Epoch_ID')
+    worksheet.write(0, 2, 'TimeStamp')
+    worksheet.write(0, 3, 'Sleep_Stage')
+    worksheet.write(0, 4, 'dateOfSleep')
+    worksheet.write(0, 5, 'startTime')
+    worksheet.write(0, 6, 'endTime')
+    worksheet.write(0, 7, 'minutesToFallAsleep')
+    worksheet.write(0, 8, 'minutesAsleep')
+    worksheet.write(0, 9, 'minutesAwake')
+    worksheet.write(0, 10, 'minutesAfterWakeup')
+    worksheet.write(0, 11, 'timeInBed')
+    worksheet.write(0, 12, 'efficiency')
+    worksheet.write(0, 13, 'deep.count')
+    worksheet.write(0, 14, 'deep.minutes')
+    worksheet.write(0, 15, 'wake.count')
+    worksheet.write(0, 16, 'wake.minutes')
+    worksheet.write(0, 17, 'light.count')
+    worksheet.write(0, 18, 'light.minutes')
+    worksheet.write(0, 19, 'rem.count')
+    worksheet.write(0, 20, 'rem.minutes')
+    worksheet.write(0, 21, 'SleepScore')
+    worksheet.write(0, 22, "CaloriesBurned")
+    worksheet.write(0, 23, "Steps")
+    worksheet.write(0, 24, "Distance")
+    worksheet.write(0, 25, "Floors")
+    worksheet.write(0, 26, "Age")
+    worksheet.write(0, 27, "BMI")
+    worksheet.write(0, 28, 'stress_1')
+    worksheet.write(0, 29, 'stress_2')
+    worksheet.write(0, 30, 'stress_3')
+    worksheet.write(0, 31, 'Temp_Med')
+    worksheet.write(0, 32, 'Abertura de Apps (Total)')
+    worksheet.write(0, 33, 'Notificacoes (Total)')
+    worksheet.write(0, 34, 'Desbloqueios (Total)')
+    worksheet.write(0, 35, 'Tela 0-3am')
+    worksheet.write(0, 36, 'Tela 3-6am')
+    worksheet.write(0, 37, 'Tela 6-9am')
+    worksheet.write(0, 38, 'Tela 9am-12pm')
+    worksheet.write(0, 39, 'Tela 12-3pm')
+    worksheet.write(0, 40, 'Tela 3-6pm')
+    worksheet.write(0, 41, 'Tela 6-9pm')
+    worksheet.write(0, 42, 'Tela 9pm-0am')
+    worksheet.write(0, 43, 'Email1')
+    worksheet.write(0, 44, 'Email2')
+    worksheet.write(0, 45, 'Email3')
+    worksheet.write(0, 46, 'Email4')
+    worksheet.write(0, 47, 'call1')
+    worksheet.write(0, 48, 'call2')
+    worksheet.write(0, 49, 'call3')
+    worksheet.write(0, 50, 'Text1')
+    worksheet.write(0, 51, 'Text2')
+    worksheet.write(0, 52, 'Text3')
+    worksheet.write(0, 53, 'Text4')
+
+
+def ConverterStrParaData(data):
+    data = data.split('T')
+    dataD = data[0].split('-')
+    dataH = data[1].split(':')
+    dataS = dataH[2].split('.')
+    return datetime(year=int(dataD[0]), month=int(dataD[1]), day=int(dataD[2]), hour=int(dataH[0]),
+                    minute=int(dataH[1]), second=int(dataS[0]))
+
+
+def getSleepData(sleepData, data):
+    for item in sleepData:
+        dataTest = item['dateOfSleep']
+        dataTest = dataTest.replace('-', ' ')
+        dataTest = reverseText(dataTest)
+        dataTest = dataTest.replace(' ', '-')
+        if dataTest == data:
+            return item
+
+
+def escreverSleepStages(tempList, worksheet, startTimeD, count):
+    for i in tempList:
+
+        dateTime1 = ConverterStrParaData(i['dateTime'])
+
+        if startTimeD < dateTime1:
+            status = 0
+            if i['level'] == 'wake':
+                status = 0
+            if i['level'] == 'light':
+                status = 2
+            if i['level'] == 'deep':
+                status = 1
+            if i['level'] == 'rem':
+                status = 3
+            worksheet.write(count, 3, status)
+            break
+
+
+def escreverLevelSummary(worksheet, levelsSummary, count):
+    try:
+        worksheet.write(count, 13, levelsSummary['deep']['count'])
+        worksheet.write(count, 14, levelsSummary['deep']['minutes'])
+    except:
+        print()
+    try:
+        worksheet.write(count, 15, levelsSummary['wake']['count'])
+        worksheet.write(count, 16, levelsSummary['wake']['minutes'])
+    except:
+        print()
+    try:
+        worksheet.write(count, 17, levelsSummary['light']['count'])
+        worksheet.write(count, 18, levelsSummary['light']['minutes'])
+    except:
+        print()
+    try:
+        worksheet.write(count, 19, levelsSummary['rem']['count'])
+        worksheet.write(count, 20, levelsSummary['rem']['minutes'])
+    except:
+        print()
+
+
+def GetSleepScore(dataSleepScore, data):
+    for item in dataSleepScore:
+        dataTest = item['timestamp']
+        dataTest = dataTest.split('T')
+        dataTest[0] = dataTest[0].replace('-', ' ')
+        dataTest[0] = reverseText(dataTest[0])
+        dataTest[0] = dataTest[0].replace(' ', '-')
+        if dataTest[0] == data:
+            return item['overall_score']
+
+
+def GetStressDoDia(dataStress, data):
+    for item in dataStress:
+        dataTest = item['Data']
+        dataTest = dataTest.replace('/', '-')
+        if dataTest == data:
+            return item
+
+
+def GetDatasetDoDia(datasetDoDia, data):
+    for item in datasetDoDia:
+        dataTest = item['Dia']
+        dataTest = dataTest.replace('/', '-')
+        if dataTest == data:
+            return item
+
+
+def GerarDataSetFinal():
+    file_list = glob.glob("*.xlsx")
+    file_list = sorted(file_list, key=lambda t: os.stat(t).st_mtime)
+    excl_list = []
+
+    for file in file_list:
+        excl_list.append(pd.read_excel(file))
+
+    excl_merged = pd.DataFrame()
+
+    for excl_file in excl_list:
+        # appends the data into the excl_merged
+        # dataframe.
+        excl_merged = excl_merged.append(
+            excl_file, ignore_index=True)
+
+    excl_merged.to_excel('Final_Dataset.xlsx', index=False)
--- main.py
+++ main.py
@@ -1,48 +1,9 @@
+from Utils import GetValidDatas, EscreverCabecalho, ConverterStrParaData, getSleepData, escreverSleepStages, escreverLevelSummary, GetSleepScore, GetStressDoDia, GetDatasetDoDia, GerarDataSetFinal
 import json
 import csv
 import xlsxwriter as xl
 from datetime import datetime
 from datetime import timedelta
-from collections import deque
-import glob
-import pandas as pd
-import openpyxl
-
-listaExcel = []
-
-
-def reverseText(s):
-    # base case
-    if not s:
-        return s
-
-    # `s[low…high]` forms a word
-    low = high = 0
-
-    # create an empty stack
-    stack = deque()
-
-    # scan the text
-    for i, c in enumerate(s):
-        # if space is found, we found a word
-        if c == ' ':
-            # push each word into the stack
-            stack.append(s[low:high + 1])
-
-            # reset `low` and `high` for the next word
-            low = high = i + 1
-        else:
-            high = i
-
-    # push the last word into the stack
-    stack.append(s[low:])
-
-    # construct the string by following the LIFO order
-    sb = ""
-    while stack:
-        sb += stack.pop() + ' '
-
-    return sb[:-1]  # remove last space
 
 
 fSleep = open('Sleep/sleep-2023-01-17.json')
@@ -60,300 +21,136 @@ fileEvery = open("Dataset.csv", "r")
 dataDataSet = list(csv.DictReader(fileEvery))
 fileStress.close()
 
-row = 1
-column = 0
+firstCalorias = [2319, 2248, 2525, 2649, 2604, 2240, 2944, 2219, 2428, 3017, 2350, 2444, 2397, 2452, 2379]
 
-firstDatas = ["2023-01-19", "2023-01-20", "2023-01-21", "2023-01-22", "2023-01-23", "2023-01-24", "2023-01-25",
-              "2023-01-26", "2023-01-27", "2023-01-28", "2023-01-29", "2023-01-30", "2023-01-31", "2023-02-01",
-              "2023-02-02"]
+firstPassos = [3400, 3287, 6463, 6874, 6765, 2856, 9996, 3081, 5048, 13814, 4299, 4905, 3417, 5074,4314]
 
-# firstCalorias = []
+# firstHeartRate = [61, 63, 63, 65, 65, 66, 67, 65, 64, 62, 60, 62, 63, 63, 63]
 
-# firstPassos = []
+firstDistance = [2.47, 2.39, 4.66, 4.96, 4.95, 2.07, 7.26, 2.24, 3.59, 10.04, 3.11, 3.56, 2.48, 3.68, 3.14]
 
-# firstHeartRate = []
+firstFloors = [8, 2, 9, 7, 21, 1, 11, 0, 8, 22, 9, 5, 2, 17, 15]
 
-# firstDistance = []
+firstAge = 20
 
-# firstFloors = []
+fistBMI = 24.1
 
-# firstAge =
+firstDatas = GetValidDatas(dataSleepScore)
+dataSleep.reverse()
+dataSleepScore.reverse()
 
-# fistBMI = KG/H
+dayCounter = 0
 
-dias = len(firstDatas)
-sleepStages = {
-    'wake': 0,
-    'deep': 1,
-    'light': 2,
-    'rem': 3
-}
+print("A gerar ficheiros...")
 
-print("Generating excel...")
-print("0%", end="")
+while dayCounter < len(firstDatas):
 
-index = 0
-for dia in firstDatas:
-    tempList = []
-    dataS = dataSleep[index]['dateOfSleep']
+    sleepData = getSleepData(dataSleep, firstDatas[dayCounter])
+    tempList =sleepData['levels']['data']
 
-    while dataS != dia:
-        dataS = dataSleep[index]['dateOfSleep']
-        index += 1
-
-    tempList = dataSleep[index]['levels']['data']
-    nome = dataSleep[index]['dateOfSleep'] + '.xlsx'
-
+    nome = firstDatas[dayCounter] + '.xlsx'
     workbook = xl.Workbook(nome)
     worksheet = workbook.add_worksheet()
 
-    worksheet.write(0, 0, 'Dia_Sessao')
-    worksheet.write(0, 1, 'Epoch_ID')
-    worksheet.write(0, 2, 'TimeStamp')
-    worksheet.write(0, 3, 'Sleep_Stage')
-    worksheet.write(0, 4, 'dateOfSleep')
-    worksheet.write(0, 5, 'startTime')
-    worksheet.write(0, 6, 'endTime')
-    worksheet.write(0, 7, 'minutesToFallAsleep')
-    worksheet.write(0, 8, 'minutesAsleep')
-    worksheet.write(0, 9, 'minutesAwake')
-    worksheet.write(0, 10, 'minutesAfterWakeup')
-    worksheet.write(0, 11, 'timeInBed')
-    worksheet.write(0, 12, 'efficiency')
-    worksheet.write(0, 13, 'deep.count')
-    worksheet.write(0, 14, 'deep.minutes')
-    worksheet.write(0, 15, 'wake.count')
-    worksheet.write(0, 16, 'wake.minutes')
-    worksheet.write(0, 17, 'light.count')
-    worksheet.write(0, 18, 'light.minutes')
-    worksheet.write(0, 19, 'rem.count')
-    worksheet.write(0, 20, 'rem.minutes')
-    worksheet.write(0, 21, 'SleepScore')
-    worksheet.write(0, 22, 'stress_1')
-    worksheet.write(0, 23, 'stress_2')
-    worksheet.write(0, 24, 'stress_3')
-    worksheet.write(0, 25, 'Temp_Med')
-    worksheet.write(0, 26, 'Abertura de Apps (Total)')
-    worksheet.write(0, 27, 'Notificacoes (Total)')
-    worksheet.write(0, 28, 'Desbloqueios (Total)')
-    worksheet.write(0, 29, 'Tela 0-3am')
-    worksheet.write(0, 30, 'Tela 3-6am')
-    worksheet.write(0, 31, 'Tela 6-9am')
-    worksheet.write(0, 32, 'Tela 9am-12pm')
-    worksheet.write(0, 33, 'Tela 12-3pm')
-    worksheet.write(0, 34, 'Tela 3-6pm')
-    worksheet.write(0, 35, 'Tela 6-9pm')
-    worksheet.write(0, 36, 'Tela 9pm-0am')
-    worksheet.write(0, 37, 'Email1')
-    worksheet.write(0, 38, 'Email2')
-    worksheet.write(0, 39, 'Email3')
-    worksheet.write(0, 40, 'Email4')
-    worksheet.write(0, 41, 'call1')
-    worksheet.write(0, 42, 'call2')
-    worksheet.write(0, 43, 'call3')
-    worksheet.write(0, 44, 'Text1')
-    worksheet.write(0, 45, 'Text2')
-    worksheet.write(0, 46, 'Text3')
-    worksheet.write(0, 47, 'Text4')
+    EscreverCabecalho(worksheet)
 
-    dia = firstDatas.index(dia) + 1
-    worksheet.write(1, 0, dia)
-
-    startSleep = ""
-    endSleep = ""
-    sleepScore = 0
-    for data in dataSleep:
-        if data['dateOfSleep'] == dataS:
-            startSleep = data['startTime']
-            endSleep = data['endTime']
-
-    for score in dataSleepScore:
-        new = score['timestamp'].split('T')
-        if new[0] == dataS:
-            sleepScore = score['overall_score']
-
-    stressDoDia = {}
-
-    for stress in dataStress:
-        new = stress['Data'].replace('/', ' ')
-        new = reverseText(new)
-        new = new.replace(' ', '-')
-        if new == dataS:
-            stressDoDia = stress
-
-    datasetDoDia = {}
-
-    for dataset in dataDataSet:
-        new = dataset['Dia'].replace('/', ' ')
-        new = reverseText(new)
-        new = new.replace(' ', '-')
-        if new == dataS:
-            datasetDoDia = dataset
-
-    startTime = startSleep.split('T')[1].split(':')
-    endTime = endSleep.split('T')[1].split(':')
-
-    startDate = startSleep.split('T')[0].split('-')
-    endDate = endSleep.split('T')[0].split('-')
-
-    startTimeD = datetime(year=int(startDate[0]), month=int(startDate[1]), day=int(startDate[2]),
-                          hour=int(startTime[0]), minute=int(startTime[1]), second=int(startTime[2].split('.')[0]))
-    endTimeD = datetime(year=int(endDate[0]), month=int(endDate[1]), day=int(endDate[2]), hour=int(endTime[0]),
-                        minute=int(endTime[1]), second=int(endTime[2].split('.')[0]))
-
-    count = 1
     format1 = workbook.add_format({'num_format': 'hh:mm'})
     format3 = workbook.add_format({'num_format': 'yyyy-mm-ddThh:mm:ss'})
     format5 = workbook.add_format({'num_format': 'hh:mm:ss'})
 
-    temp = dataSleep[index]['levels']['summary']
+    startSleep = sleepData['startTime']
+    endSleep = sleepData['endTime']
 
-    try:
-        if temp['deep']['count'] != 0 and temp['wake']['count'] != 0 and temp['light']['count'] != 0 and temp['rem'][
-            'count'] != 0:
-            while startTimeD < endTimeD:
-                worksheet.write(count, 0, dia)
-                worksheet.write(count, 1, count)
-                worksheet.write_datetime(count, 2, startTimeD, format5)
-                startTimeD = startTimeD + timedelta(seconds=30)
+    startSleepData = ConverterStrParaData(startSleep)
+    endSleepData = ConverterStrParaData(endSleep)
+    row = 1
+    while startSleepData < endSleepData:
 
-                for i in tempList:
-                    time1 = i['dateTime'].split('T')
-                    time2 = time1[0].split('-')
-                    time3 = time1[1].split(':')
+        worksheet.write(row, 0, dayCounter+1) # dia
+        worksheet.write(row, 1, row) # epoch
+        worksheet.write(row, 2, startSleepData, format5)
 
-                    dateTime1 = datetime(year=int(time2[0]), month=int(time2[1]), day=int(time2[2]),
-                                         hour=int(time3[0]), minute=int(time3[1]), second=int(time3[2].split('.')[0]))
+        escreverSleepStages(tempList, worksheet, startSleepData, row)
 
-                    if startTimeD < dateTime1:
-                        status = 0
-                        if i['level'] == 'wake':
-                            status = 0
-                        if i['level'] == 'light':
-                            status = 2
-                        if i['level'] == 'deep':
-                            status = 1
-                        if i['level'] == 'rem':
-                            status = 3
-                        worksheet.write(count, 3, status)
-                        break
+        worksheet.write(row, 4, startSleepData.strftime("%d/%m/%Y"))
 
-                worksheet.write(count, 4, startTimeD.strftime("%d/%m/%Y"))
-                startTimeD1 = datetime(year=int(startDate[0]), month=int(startDate[1]), day=int(startDate[2]),
-                                       hour=int(startTime[0]), minute=int(startTime[1]),
-                                       second=int(startTime[2].split('.')[0]))
-                worksheet.write_datetime(count, 5, startTimeD1, format3)
-                endTimeD1 = datetime(year=int(endDate[0]), month=int(endDate[1]), day=int(endDate[2]),
-                                     hour=int(endTime[0]), minute=int(endTime[1]), second=int(endTime[2].split('.')[0]))
-                worksheet.write_datetime(count, 6, endTimeD1, format3)
+        worksheet.write_datetime(row, 5, startSleepData, format3)
+        worksheet.write_datetime(row, 6, endSleepData, format3)
 
-                worksheet.write(count, 7, dataSleep[index]['minutesToFallAsleep'])
-                worksheet.write(count, 8, dataSleep[index]['minutesAsleep'])
-                worksheet.write(count, 9, dataSleep[index]['minutesAwake'])
-                worksheet.write(count, 10, dataSleep[index]['minutesAfterWakeup'])
-                worksheet.write(count, 11, dataSleep[index]['timeInBed'])
-                worksheet.write(count, 12, dataSleep[index]['efficiency'])
+        worksheet.write(row, 7, sleepData['minutesToFallAsleep'])
+        worksheet.write(row, 8, sleepData['minutesAsleep'])
+        worksheet.write(row, 9, sleepData['minutesAwake'])
+        worksheet.write(row, 10, sleepData['minutesAfterWakeup'])
+        worksheet.write(row, 11, sleepData['timeInBed'])
+        worksheet.write(row, 12, sleepData['efficiency'])
 
-                levelsSummary = dataSleep[index]['levels']['summary']
-                try:
-                    worksheet.write(count, 13, levelsSummary['deep']['count'])
-                    worksheet.write(count, 14, levelsSummary['deep']['minutes'])
-                except:
-                    print()
-                try:
-                    worksheet.write(count, 15, levelsSummary['wake']['count'])
-                    worksheet.write(count, 16, levelsSummary['wake']['minutes'])
-                except:
-                    print()
-                try:
-                    worksheet.write(count, 17, levelsSummary['light']['count'])
-                    worksheet.write(count, 18, levelsSummary['light']['minutes'])
-                except:
-                    print()
-                try:
-                    worksheet.write(count, 19, levelsSummary['rem']['count'])
-                    worksheet.write(count, 20, levelsSummary['rem']['minutes'])
-                except:
-                    print()
-                worksheet.write(count, 21, int(sleepScore))
+        levelsSummary = sleepData['levels']['summary']
 
-                worksheet.write(count, 22, int(stressDoDia['Stress1']))
-                worksheet.write(count, 23, int(stressDoDia['Stress2']))
-                worksheet.write(count, 24, int(stressDoDia['Stress3']))
+        escreverLevelSummary(worksheet, levelsSummary, row)
 
-                worksheet.write(count, 25, int(datasetDoDia['Temperatura Media do ar']))
-                worksheet.write(count, 26, int(datasetDoDia['Abertura de Apps (Total)']))
-                worksheet.write(count, 27, int(datasetDoDia['Notificacoes (Total)']))
-                worksheet.write(count, 28, int(datasetDoDia['Desbloqueios (Total)']))
-                worksheet.write(count, 29, int(datasetDoDia['0-3am']))
-                worksheet.write(count, 30, int(datasetDoDia['3-6am']))
-                worksheet.write(count, 31, int(datasetDoDia['6-9am']))
-                worksheet.write(count, 32, int(datasetDoDia['9am-12pm']))
-                worksheet.write(count, 33, int(datasetDoDia['12-3pm']))
-                worksheet.write(count, 34, int(datasetDoDia['3-6pm']))
-                worksheet.write(count, 35, int(datasetDoDia['6-9pm']))
-                worksheet.write(count, 36, int(datasetDoDia['9pm-0am']))
-                worksheet.write(count, 37, int(datasetDoDia['Enviados (Total)']))
-                worksheet.write(count, 38, int(datasetDoDia['Recebidos (Total)']))
+        worksheet.write(row, 21, int(GetSleepScore(dataSleepScore, firstDatas[dayCounter])))
 
-                worksheet.write(count, 39, datasetDoDia['1 email enviado (timestamp)'])
-                worksheet.write(count, 40, datasetDoDia['ultimo email enviado (timestamp)'])
-                try:
-                    worksheet.write(count, 41, int(datasetDoDia['Total (efetuadas + recebidas)']))
-                except:
-                    print()
-                worksheet.write(count, 42, datasetDoDia['1 chamada (timestamp)'])
-                worksheet.write(count, 43, datasetDoDia['ultima chamada (timestamp)'])
-                try:
-                    worksheet.write(count, 44, int(datasetDoDia['Total (recebidas + enviadas)']))
-                    worksheet.write(count, 45, int(datasetDoDia['Total apos 0h (recebidas + enviadas)']))
-                except:
-                    print()
-                worksheet.write(count, 46, datasetDoDia['1 mensagem (timestamp)'])
-                worksheet.write(count, 47, datasetDoDia['ultima mensagem (timestamp)'])
+        worksheet.write(row, 22, firstCalorias[dayCounter])
 
-                count += 1
-            index = 0
+        worksheet.write(row, 23, firstPassos[dayCounter])
 
-            workbook.close()
-    except:
-        index = 0
-        print()
+        worksheet.write(row, 24, firstDistance[dayCounter])
 
-    print("=", end="")
+        worksheet.write(row, 25, firstFloors[dayCounter])
 
-print("100%")
+        worksheet.write(row, 26, firstAge)
 
-print("Agrupando e organizando ficheiros resultantes da operação anterior...")
-print("0%", end="")
-print("=", end="")
+        worksheet.write(row, 27, fistBMI)
 
-file_list = glob.glob("*.xlsx")
+        stressDoDia = GetStressDoDia(dataStress, firstDatas[dayCounter])
 
-excl_list = []
+        worksheet.write(row, 28, int(stressDoDia['Stress1']))
+        worksheet.write(row, 29, int(stressDoDia['Stress2']))
+        worksheet.write(row, 30, int(stressDoDia['Stress3']))
 
-for file in file_list:
-    excl_list.append(pd.read_excel(file))
-    print("=", end="")
+        datasetDoDia = GetDatasetDoDia(dataDataSet, firstDatas[dayCounter])
 
-print("100%")
-print("Gerando Dataset Final...")
-print("0%", end="")
+        worksheet.write(row, 31, int(datasetDoDia['Temperatura Media do ar']))
+        worksheet.write(row, 32, int(datasetDoDia['Abertura de Apps (Total)']))
+        worksheet.write(row, 33, int(datasetDoDia['Notificacoes (Total)']))
+        worksheet.write(row, 34, int(datasetDoDia['Desbloqueios (Total)']))
+        worksheet.write(row, 35, int(datasetDoDia['0-3am']))
+        worksheet.write(row, 36, int(datasetDoDia['3-6am']))
+        worksheet.write(row, 37, int(datasetDoDia['6-9am']))
+        worksheet.write(row, 38, int(datasetDoDia['9am-12pm']))
+        worksheet.write(row, 39, int(datasetDoDia['12-3pm']))
+        worksheet.write(row, 40, int(datasetDoDia['3-6pm']))
+        worksheet.write(row, 41, int(datasetDoDia['6-9pm']))
+        worksheet.write(row, 42, int(datasetDoDia['9pm-0am']))
+        worksheet.write(row, 43, int(datasetDoDia['Enviados (Total)']))
+        worksheet.write(row, 44, int(datasetDoDia['Recebidos (Total)']))
 
-# create a new dataframe to store the
-# merged excel file.
-excl_merged = pd.DataFrame()
+        worksheet.write(row, 45, datasetDoDia['1 email enviado (timestamp)'])
+        worksheet.write(row, 46, datasetDoDia['ultimo email enviado (timestamp)'])
 
-for excl_file in excl_list:
-    # appends the data into the excl_merged
-    # dataframe.
-    excl_merged = excl_merged.append(
-        excl_file, ignore_index=True)
-    print("=", end="")
-# exports the dataframe into excel file with
-# specified name.
-excl_merged.to_excel('Final_Dataset.xlsx', index=False)
+        try:
+            worksheet.write(row, 47, int(datasetDoDia['Total (efetuadas + recebidas)']))
+        except:
+            print()
+        worksheet.write(row, 48, datasetDoDia['1 chamada (timestamp)'])
+        worksheet.write(row, 49, datasetDoDia['ultima chamada (timestamp)'])
+        try:
+            worksheet.write(row, 50, int(datasetDoDia['Total (recebidas + enviadas)']))
+            worksheet.write(row, 51, int(datasetDoDia['Total apos 0h (recebidas + enviadas)']))
+        except:
+            print()
+        worksheet.write(row, 52, datasetDoDia['1 mensagem (timestamp)'])
+        worksheet.write(row, 53, datasetDoDia['ultima mensagem (timestamp)'])
 
-print("100%")
+        row += 1
+        startSleepData = startSleepData + timedelta(seconds=30)
 
-print("Programa terminado")
+    workbook.close()
+
+    dayCounter += 1
+
+print("Ficheiros gerados.")
+
+print("A gerar dataset final...")
+GerarDataSetFinal()
+print("Dataset final gerado.")
